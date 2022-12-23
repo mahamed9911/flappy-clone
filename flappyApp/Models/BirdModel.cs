24cbbbb096e224fbc5e4309c7c8bc1cd6e5adae9
Mahamed Aweis <81790372+mahamed9911@users.noreply.github.com>
2022-12-23 16:54:49 -0500

    Add files via upload

---
+++ flappyApp/Models/BirdModel.cs
@@ -0,0 +1,26 @@
+﻿namespace FlappyPlane.Web.Game.Models
+{
+    public class BirdModel
+    {
+        public int DistanceFromGround { get; set; } = 100;
+        public int JumpStrength { get; set; } = 50;
+
+        public bool IsOnGround()
+        {
+            return DistanceFromGround <= 0;
+        }
+
+        public void Fall(int gravity)
+        {
+            DistanceFromGround -= gravity;
+        }
+
+        public void Jump()
+        {
+            if(DistanceFromGround <= 520)
+            {
+                DistanceFromGround += JumpStrength;
+            }
+        }
+    }
+}
---
+++ flappyApp/Models/GameManager.cs
@@ -0,0 +1,99 @@
+﻿
+namespace FlappyPlane.Web.Game.Models
+{
+    public class GameManager
+    {
+        private readonly int _gravity = 2;
+
+        public event EventHandler? MainLoopCompleted;
+
+        public BirdModel Bird { get; set; }
+        public List<PipeModel> Pipes { get; set; }
+        public bool IsRunning { get; set; } = false;
+
+        public GameManager()
+        {
+            Bird = new BirdModel();
+            Pipes = new List<PipeModel>();
+        }
+
+        public async void MainLoop()
+        {
+            IsRunning = true;
+            while(IsRunning)
+            {
+                MoveObjects();
+                Collision();
+                ManagePipes();
+
+                MainLoopCompleted?.Invoke(this, EventArgs.Empty);
+                
+                await Task.Delay(20);
+            }
+        }
+
+        public void StartGame()
+        {
+            if (!IsRunning)
+            {
+                Bird = new BirdModel();
+                Pipes = new List<PipeModel>();
+                MainLoop();
+            }       
+        }
+
+        public void Jump()
+        {
+            if(IsRunning)
+            {
+                Bird.Jump();
+            }
+        }
+        void Collision()
+        {
+            if (Bird.IsOnGround()) 
+            {
+                GameOver();
+            }
+
+
+            var centeredPipe = Pipes.FirstOrDefault(p => p.isCentered());
+
+            if (centeredPipe != null)
+            {
+                bool hasCollidedBottom = Bird.DistanceFromGround < centeredPipe.GapBottom - 150;
+                bool hasCollidedTop = Bird.DistanceFromGround + 45 > centeredPipe.GapTop - 150;
+
+                if (hasCollidedBottom || hasCollidedTop)
+                {
+                    GameOver();
+                }
+            }
+        }
+
+        void ManagePipes()
+        {
+            if(!Pipes.Any() || Pipes.Last().DistanceFromLeft <= 250)
+            {
+                Pipes.Add(new PipeModel());
+            }
+            if(Pipes.First().OffScreen())
+            {
+                Pipes.Remove(Pipes.First());
+            }
+        }
+        void MoveObjects()
+        {
+            Bird.Fall(_gravity);
+            foreach (var pipe in Pipes)
+            {
+                pipe.Move();
+            }
+        }
+
+        public void GameOver()
+        {
+            IsRunning = false;
+        }
+    }
+}
